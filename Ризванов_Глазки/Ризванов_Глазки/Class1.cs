2e5307ca8e4b51406faa1b341867f1a1ac5d586d
RizvanovIlnaz <riz.ilnaz2005@gmail.com>
2023-11-03 14:44:14 +0500

    lab 10

---
+++ Ризванов_Глазки/Ризванов_Глазки/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ризванов_Глазки
+{
+    internal class Class1
+    {
+    }
+}
---
+++ Ризванов_Глазки/Ризванов_Глазки/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Ризванов_Глазки
+{
+    class Manager
+    {
+        public static Frame MainFarame { get; set; }
+    }
+}
