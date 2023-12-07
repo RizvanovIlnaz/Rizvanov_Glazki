01bd33fbc180e47282e877590de8eb45f67ef71f
RizvanovIlnaz <riz.ilnaz2005@gmail.com>
2023-12-07 16:28:10 +0500

    13

---
+++ Ризванов_Глазки1/Ризванов_Глазки/AddEditPage.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace Ризванов_Глазки
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+       private Agent currentAgent = new Agent();
+
+        public bool check = false;
+        public AddEditPage(Agent SelectedAgent)
+        {
+            InitializeComponent();
+
+            if (SelectedAgent != null)
+            {
+                check = true;
+                currentAgent = SelectedAgent;
+            }
+
+            DataContext = currentAgent;
+        }
+       
+
+        private void ChangePictureBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog myOpenFileDialog = new OpenFileDialog();
+            if (myOpenFileDialog.ShowDialog() == true)
+            {
+                currentAgent.Logo = myOpenFileDialog.FileName;
+                LogoImage.Source = new BitmapImage(new Uri(myOpenFileDialog.FileName));
+            }
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(currentAgent.Title))
+                errors.AppendLine("Укажите наименование агента");
+            if (string.IsNullOrWhiteSpace(currentAgent.Address))
+                errors.AppendLine("Укажите адрес агента");
+            if (string.IsNullOrWhiteSpace(currentAgent.DirectorName))
+                errors.AppendLine("Укажите ФИО директора");
+            if (ComboType.SelectedItem == null)
+                errors.AppendLine("Укажите тип агента");
+            if (string.IsNullOrWhiteSpace(currentAgent.Priority.ToString()))
+                errors.AppendLine("Укажите приоритет агента");
+            if (currentAgent.Priority <= 0)
+                errors.AppendLine("Укажите положительный приоритет агента");
+            if (string.IsNullOrWhiteSpace(currentAgent.INN))
+                errors.AppendLine("Укажите ИНН агента");
+            if (string.IsNullOrWhiteSpace(currentAgent.KPP))
+                errors.AppendLine("Укажите КПП агента");
+            if (string.IsNullOrWhiteSpace(currentAgent.Phone))
+                errors.AppendLine("Укажите телефон агента");
+            else
+            {
+                string ph = currentAgent.Phone.Replace("(", "").Replace(")", "").Replace(" ", "").Replace("-", "").Replace("+", "");
+                if (((ph[1] == '9' || ph[1] == '4' || ph[1] == '8') && ph.Length != 11) || (ph[1] == '3' && ph.Length != 12))
+                    errors.AppendLine("Укажите правильно телефон агента");
+            }
+            if (string.IsNullOrWhiteSpace(currentAgent.Email))
+                errors.AppendLine("Укажите почту агента");
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+           
+            currentAgent.AgentTypeID = ComboType.SelectedIndex + 1;
+
+            var allAgent = Ризванов_ГлазкиEntities.GetContext().Agent.ToList();
+            allAgent = allAgent.Where(p => p.Title == currentAgent.Title).ToList();
+
+            if (allAgent.Count == 0 || check == true)
+            {
+                if (currentAgent.ID == 0)
+                    Ризванов_ГлазкиEntities.GetContext().Agent.Add(currentAgent);
+                try
+                {
+                    Ризванов_ГлазкиEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Информация сохранена");
+                    Manager.MainFarame.GoBack();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+            else
+                MessageBox.Show("Уже существует такая услуга");
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var currentAgent = (sender as Button).DataContext as Agent;
+
+            var currentAgentList = Ризванов_ГлазкиEntities.GetContext().ProductSale.ToList();
+            currentAgentList = currentAgentList.Where(p => p.AgentID == currentAgent.ID).ToList();
+            if (currentAgentList.Count != 0)
+            {
+                MessageBox.Show("Невозможно выполнить удаление, так как существуют записи на эту услугу");
+            }
+            else
+            {
+                if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        Ризванов_ГлазкиEntities.GetContext().Agent.Remove(currentAgent);
+                        Ризванов_ГлазкиEntities.GetContext().SaveChanges();
+                        Manager.MainFarame.GoBack();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
